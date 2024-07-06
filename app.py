389c989aae15fde49add411c73f07751edceec1d
kristinang12 <kristinangalcober12@gmail.com>
2024-07-06 09:55:52 +0800

    edit app

--- app.py
+++ app.py
@@ -24,4 +24,7 @@ def calculate():
         return "Invalid input, please enter numerical values."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable if available, otherwise default to 8080
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
